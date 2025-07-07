3128e9a8f7f5e3b9bd1683a52a6cc770194d12cc
franlamy2117 <francois.lam@mahidol.edu>
2025-07-07 10:57:43 +0700

    Update ChiSquare_Heatmaps.py

--- ThaiAlcoholTweets2023/ChiSquare_Heatmaps.py
+++ ThaiAlcoholTweets2023/ChiSquare_Heatmaps.py
@@ -3,18 +3,23 @@ from scipy.stats import chi2_contingency
 from scipy.stats import kruskal
 import numpy as np
 
-def chi_square_by_hour(df, df2):
-    df_ref = pd.read_csv(df)
-    df_subset = pd.read_csv(df2)
+def chi_square_by_hour(pc_net, pc_pos, pc_neg):
+    df_ref =  pc_net.copy()
+    df_sub1 = pc_pos.copy()
+    df_sub2 = pc_neg.copy()
     df2 = pd.DataFrame()
     df2['datetime'] = pd.to_datetime(df_ref['created_at'], utc=True, errors='coerce')
     df2['datetime'] = df2['datetime'].dt.tz_convert("Asia/Bangkok")
     df3 = pd.DataFrame()
-    df3['datetime'] = pd.to_datetime(df_subset['created_at'], utc=True, errors='coerce')
+    df3['datetime'] = pd.to_datetime(df_sub1['created_at'], utc=True, errors='coerce')
     df3['datetime'] = df3['datetime'].dt.tz_convert("Asia/Bangkok")
+    df4 = pd.DataFrame()
+    df4['datetime'] = pd.to_datetime(df_sub2['created_at'], utc=True, errors='coerce')
+    df4['datetime'] = df4['datetime'].dt.tz_convert("Asia/Bangkok")
     ref_counts = df2['datetime'].dt.strftime('%w-%H').value_counts().sort_index()
     sub_counts = df3['datetime'].dt.strftime('%w-%H').value_counts().reindex(ref_counts.index, fill_value=0)
-    contingency = pd.concat([ref_counts, sub_counts], axis=1, keys=['All_PC', 'Subset']).fillna(0)
+    sub_counts2 = df4['datetime'].dt.strftime('%w-%H').value_counts().reindex(ref_counts.index, fill_value=0)
+    contingency = pd.concat([ref_counts, sub_counts, sub_counts2], axis=1).fillna(0)
     chi2, p, dof, expected = chi2_contingency(contingency.T)
     print(f"\nChi-square test for time distribution:")
     print(f"Chi² = {chi2:.2f}, df = {dof}, p = {p:.5f}")
@@ -27,41 +32,3 @@ def chi_square_by_hour(df, df2):
     cramer_v = np.sqrt(phi2corr / min((kcorr-1), (rcorr-1)))
     print(f"Cramer's V = {cramer_v:.4f}")
     return contingency
-
-def kruskal_wallis_by_hour(path):
-    df = pd.read_csv(path)
-    h_statistic, p_value = kruskal(df['Positive'], df['Negative'], df['Neutral'])
-    print(f"Kruskal_Wallis H-statistic: {h_statistic}")
-    print(f"Kruskal_Wallis P-value: {p_value}")
-
-    # Interpret the results
-    alpha = 0.05
-    if p_value < alpha:
-        print("Reject the null hypothesis: There are significant differences between the groups.")
-    else:
-        print("Fail to reject the null hypothesis: No significant differences found.")
-
-def chi_square_by_hour2(path1):
-    df_ref = pd.read_csv(path1)
-    ref_counts = df_ref['Neutral']
-    sub_counts = df_ref['Negative']
-    p_counts = df_ref['Positive']
-    contingency = pd.concat([ref_counts, sub_counts, p_counts], axis=1).fillna(0)
-    chi2, p, dof, expected = chi2_contingency(contingency.T)
-    print(f"\nChi-square test for time distribution:")
-    print(f"Chi² = {chi2:.2f}, df = {dof}, p = {p:.5f}")
-    n = contingency.to_numpy().sum()
-    phi2 = chi2 / n
-    r, k = contingency.shape
-    phi2corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))
-    rcorr = r - ((r-1)**2)/(n-1)
-    kcorr = k - ((k-1)**2)/(n-1)
-    cramer_v = np.sqrt(phi2corr / min((kcorr-1), (rcorr-1)))
-    print(f"Cramer's V = {cramer_v:.4f}")
-    return contingency
-
-
-# chi_square_by_hour(r'D:\CAS tweets\JPSS revision\Final\Neutral_sentiments.csv',
-#                    r'D:\CAS tweets\JPSS revision\Final\Negative_sentiments.csv')
-chi_square_by_hour2(r"D:\CAS tweets\JPSS revision\Final\chisquaredata.csv")
-kruskal_wallis_by_hour(r"D:\CAS tweets\JPSS revision\Final\chisquaredata.csv")
